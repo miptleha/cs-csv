e50f7ca3f959767951d6e4c505937ba1464806a8
Alexey Obukhov <miptleha@gmail.com>
2018-10-15 15:15:34 +0300

    store in string, more tests

--- Csv.cs
+++ Csv.cs
@@ -32,12 +32,19 @@ namespace CsvInOut
         {
             var data = new TableData();
             StreamReader sr = new StreamReader(ms, Encoding.UTF8);
-            string s = sr.ReadLine();
-            if (s != null)
-                data.Header.AddRange(s.Split(new string[] { ", " }, StringSplitOptions.None));
+            string s;
             while ((s = sr.ReadLine()) != null)
             {
-                data.Content.Add(s.Split(new string[] { ", " }, StringSplitOptions.None));
+                if (string.IsNullOrWhiteSpace(s))
+                    continue;
+                data.Header.AddRange(s.Split(new string[] { "; " }, StringSplitOptions.None));
+                break;
+            }
+            while ((s = sr.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(s))
+                    continue;
+                data.Content.Add(s.Split(new string[] { "; " }, StringSplitOptions.None));
             }
             return data;
         }
@@ -45,12 +52,40 @@ namespace CsvInOut
         public static void WriteCsv(Stream ms, TableData data)
         {
             StreamWriter sw = new StreamWriter(ms, Encoding.UTF8);
-            sw.WriteLine(String.Join(", ", data.Header));
+            sw.WriteLine(String.Join("; ", data.Header));
             foreach (var r in data.Content)
             {
-                sw.WriteLine(String.Join(", ", r));
+                sw.WriteLine(String.Join("; ", r));
             }
             sw.Flush();
         }
+
+        public static TableData ReadCsvString(string content)
+        {
+            using (var st = GenerateStreamFromString(content))
+            {
+                return ReadCsv(st);
+            }
+        }
+
+        public static string WriteCsvString(TableData data)
+        {
+            using (var ms = new MemoryStream())
+            {
+                WriteCsv(ms, data);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -31,17 +31,30 @@ namespace CsvInOut
 
                 ms.Position = 0;
                 var data1 = Csv.ReadCsv(ms);
+                Compare("WriteCsv/ReadCsv (memory stream)", data, data1);
 
-                string res = Comparer.Compare(data, data1);
-                if (res == null)
-                    Console.WriteLine("Test passed");
-                else
-                    Console.WriteLine("Test failed: " + res);
+                var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "test.csv");
+                Csv.WriteCsv(path, data);
+                var data2 = Csv.ReadCsv(path);
+                Compare("WriteCsv/ReadCsv (file)", data, data2);
+
+                string csvData = Csv.WriteCsvString(data);
+                var data3 = Csv.ReadCsvString(csvData);
+                Compare("WriteCsvString/ReadCsvString (string)", data, data3);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
+        }
+
+        private static void Compare(string test, TableData src, TableData dst)
+        {
+            string res = Comparer.Compare(src, dst);
+            if (res == null)
+                Console.WriteLine("Test " + test + " passed");
+            else
+                Console.WriteLine("Test " + test + " failed: " + res);
         }
 
         private static void WriteToConsole(MemoryStream ms)
